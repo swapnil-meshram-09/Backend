7d762a78c61ae20b3456e135edcd2a3c2f10de6b
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-06 19:35:41 +0530

    new commit

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ import express from 'express'
 
 const app = express()
 
-
 app.get('/', (req, res) =>{
 
-    console.log('Server is started.')
+    res.send('server is started.')
+
+    // console.log('Server is started.')
+})
+
+
+const port = process.env.PORT || 3000;
+
+// const port = 3000;
+
+app.listen(port, () =>{
+
+    console.log(`Serve at http://localhost:${port}`);
 })
