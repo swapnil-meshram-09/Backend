b5473eeec0e580854038ec21d1eda29e048f6526
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-09 17:05:30 +0530

    new commit

--- Backend/Server/server.js
+++ Backend/Server/server.js
@@ -2,11 +2,11 @@ import express from 'express'
 
 const app = express()
 
+const PORT = 3000
+
 app.get('/',(req,res) =>{
     res.send('server is running...')
 })
-
-const PORT = 3000
 
 app.listen(PORT,() =>{
     console.log(`Serve at http://localhost:${PORT}`); 
