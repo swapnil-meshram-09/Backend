ae6995540c7d43948bae596f01af33460733d7b4
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-09 17:26:20 +0530

    new commit

--- Backend/Server/SetTimeout.js
+++ Backend/Server/SetTimeout.js
@@ -1,15 +1,15 @@
-import express from 'express'
+// import express from 'express'
 
-const app = express()
+// const app = express()
 
-const PORT = 3000
+// const PORT = 3000
 
-app.get('/',(req,res) =>{
-    setTimeout(() =>{
-        res.send('Server is running..')
-    },2000)
-})
+// app.get('/',(req,res) =>{
+//     setTimeout(() =>{
+//         res.send('Server is running..')
+//     },2000)
+// })
 
-app.listen(PORT,() =>{
-    console.log(`Serve at http://localhost:${PORT}`);  
-})
+// app.listen(PORT,() =>{
+//     console.log(`Serve at http://localhost:${PORT}`);  
+// })
--- Backend/Server/server.js
+++ Backend/Server/server.js
@@ -1,13 +1,13 @@
-import express from 'express'
+// import express from 'express'
 
-const app = express()
+// const app = express()
 
-const PORT = 3000
+// const PORT = 3000
 
-app.get('/',(req,res) =>{
-    res.send('server is running...')
-})
+// app.get('/',(req,res) =>{
+//     res.send('server is running...')
+// })
 
-app.listen(PORT,() =>{
-    console.log(`Serve at http://localhost:${PORT}`); 
-})
+// app.listen(PORT,() =>{
+//     console.log(`Serve at http://localhost:${PORT}`); 
+// })
