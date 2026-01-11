98f7c5e90cb2d1f3ecd2c3c47e77520b7daefc33
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-11 19:11:58 +0530

    new commit

--- Backend/Mongoose/models/ecommerce/order.models.js
+++ Backend/Mongoose/models/ecommerce/order.models.js
@@ -10,6 +10,10 @@ const orderSchema = new mongoose.Schema(
             type: Number,
             required: true,
         },
+        stock: {
+            type: Number,
+            required: true,
+        },
         
     }, {timestamps: true})
 
