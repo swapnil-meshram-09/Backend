038b8f6f4c1f7366896e90424f079a516a15e523
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-11 19:11:21 +0530

    new commit

--- Backend/Mongoose/models/ecommerce/order.models.js
+++ Backend/Mongoose/models/ecommerce/order.models.js
@@ -2,6 +2,11 @@ import mongoose from 'mongoose'
 
 const orderSchema = new mongoose.Schema(
     {
+        name: {
+            type: String,
+            required: true
+        },
+        
     }, {timestamps: true})
 
 export const Order = monggose.model('Order',orderSchema)
