25e281306e26ee1afdd85e5bed8bb56f0a843b36
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-11 19:04:41 +0530

    new commit

--- Backend/Mongoose/models/ecommerce/product.models.js
+++ Backend/Mongoose/models/ecommerce/product.models.js
@@ -11,6 +11,10 @@ const productSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
+        price:{
+            type: Number,
+            required: true,
+        },
         
     },{timestamps:true})
 
