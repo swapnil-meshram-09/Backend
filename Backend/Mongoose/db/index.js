01560bd40977a20a68a006f39093bb2b3c768d94
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-12 19:44:55 +0530

    new commit

--- Backend/Mongoose/db/index.js
+++ Backend/Mongoose/db/index.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose'
 
+
 (async function dbConnect() {
     try{
-
+        mongoose.connect()
     }catch(error){
         console.log(error);
-        
     }
 
 })()
