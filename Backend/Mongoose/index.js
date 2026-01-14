a6a0c05c72bc7428587148e9db227f7cf6b3c1a3
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-14 18:30:39 +0530

    new commit

--- Backend/Mongoose/index.js
+++ Backend/Mongoose/index.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose'
 import DB_NAME from '../Mongoose/constants.js'
 
-async function connectDB (){
+;(async function connectDB (){
     try{
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
     }catch(error){
         console.log(error);
         
     }
-}
+})()
