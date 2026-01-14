1a59c3158f64ed89f21bdbcbc0c942a71bfccfc7
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-14 19:11:39 +0530

    new commit

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -4,8 +4,8 @@ import { DB_NAME } from '../constants.js'
 const connectDB = async () =>{
     try{
         const connected = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-
-        console.log(`DB Connection: ${connected.Connection.host}`)
+        console.log('MONGODB_URI:', process.env.MONGODB_URI);
+        console.log(`DB Connection: ${connected.connection.host}`)
         
     } catch(error){
         console.log(error);
