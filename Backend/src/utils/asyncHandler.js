b5cba9325133c3058d1ed43a8590814f56454a58
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-15 19:44:19 +0530

    new commit

--- Backend/src/utils/asyncHandler.js
+++ Backend/src/utils/asyncHandler.js
@@ -4,8 +4,11 @@ const asyncHandler = () => async (req,res,next) => {
     try{
 
     }catch(error){
-        console.log(error);
-        
+        // console.log(error);
+        res.status(error.code || 500).json({
+            sucees: false,
+            message: error.message
+        })
     }
 }
 
