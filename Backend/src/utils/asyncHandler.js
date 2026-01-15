bdde4106f1ed63a9872a04c5a518b4df462a6a31
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-15 19:45:00 +0530

    new commit

--- Backend/src/utils/asyncHandler.js
+++ Backend/src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
-import { log } from "node:console"
 
-const asyncHandler = () => async (req,res,next) => {
+const asyncHandler = (fn) => async (req,res,next) => {
     try{
+        await fn(req,res,next)
 
     }catch(error){
         // console.log(error);
