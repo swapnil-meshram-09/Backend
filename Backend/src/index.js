e940dd9ae3c25f779136056d8cf2831d4980c9e0
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-15 18:53:07 +0530

    new commit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,8 +1,27 @@
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
+import { app } from './app.js'
 
 dotenv.config({
     path: './env'
 })
 
+// -r dotenv/config --experimental-json-modules
+
 connectDB()
+.then(() =>{
+
+   app.on('error', (error) =>{
+       console.log(error);
+       throw error
+   })
+
+   app.listen(process.env.PORT || 4000, () =>{
+    console.log(`serve at http://localhost:${process.env.PORT}`);
+    
+   })
+})
+.catch((error) =>{
+   console.log(error);
+   
+})
