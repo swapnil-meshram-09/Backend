288482f6eaaee3d25d5b78deb1e85f1aaec82d91
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-15 20:02:33 +0530

    new commit

--- Backend/src/utils/apiError.js
+++ Backend/src/utils/apiError.js
@@ -6,7 +6,7 @@ class apiError extends Error{
         stack = ''
     ){
         super(message)
-        this.statusCode = statusCode,
-        this.data = null,
+        this.statusCode = statusCode
+        this.data = null
     }
 }
