e09e65cae60f1a3707abb452a7d8be78c9424482
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-15 19:47:50 +0530

    new commit

--- Backend/src/utils/asyncHandler.js
+++ Backend/src/utils/asyncHandler.js
@@ -1,15 +1,20 @@
 
-const asyncHandler = (fn) => async (req,res,next) => {
-    try{
-        await fn(req,res,next)
 
-    }catch(error){
-        // console.log(error);
-        res.status(error.code || 500).json({
-            sucees: false,
-            message: error.message
-        })
-    }
-}
+
+
+
+
+// const asyncHandler = (fn) => async (req,res,next) => {
+//     try{
+//         await fn(req,res,next)
+
+//     }catch(error){
+//         // console.log(error);
+//         res.status(error.code || 500).json({
+//             sucees: false,
+//             message: error.message
+//         })
+//     }
+// }
 
 export { asyncHandler }
