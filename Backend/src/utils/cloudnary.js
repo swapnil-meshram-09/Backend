3d6811bb4e1218d9e02d2f5f5a9adea4416c70f4
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-16 19:55:49 +0530

    new commit

--- Backend/src/utils/cloudnary.js
+++ Backend/src/utils/cloudnary.js
@@ -1,2 +1,10 @@
 import { v2 as cloudinary } from 'cloudinary'
 import fs from 'fs'     // file read, write, remove = file system inbulit in node
+
+cloudinary.config({ 
+  cloud_name: 'dklptxbp2', 
+  api_key: 731225411843594, 
+  api_secret: 'oDTVLPvy_T2Serdo1iEFGLHA6bs'
+});
+
+
