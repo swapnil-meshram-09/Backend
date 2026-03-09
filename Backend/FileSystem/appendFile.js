28c161008b2da6cb165c7a4315fd03a32f63fa09
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-09 20:54:47 +0530

    new commit

--- Backend/FileSystem/appendFile.js
+++ Backend/FileSystem/appendFile.js
@@ -1,5 +1,12 @@
-import fs from 'fs'
+// import fs from 'fs'
 
-fs.appendFile('FileSystem/Files/file.txt', ' Appended data in existing file', ()=>{
+// fs.appendFile('FileSystem/Files/file.txt', ' Appended data in existing file', ()=>{
+//     console.log('Data Append');
+// })
+
+
+import { appendFile } from 'fs'
+
+appendFile('FileSystem/Files/file.txt', ' new data append in file.', ()=>{
     console.log('Data Append');
 })
