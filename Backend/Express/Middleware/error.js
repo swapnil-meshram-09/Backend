8199141edc542497a9802b2804688e38259dfe22
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-10 16:35:41 +0530

    new commit

--- Backend/Express/Middleware/error.js
+++ Backend/Express/Middleware/error.js
@@ -2,12 +2,21 @@ import express from 'express'
 
 const app = express()
 
+app.use((req, res, next) =>{
+    console.log('Middle ware ');
+    next()
+})
+
 app.get('/', (req, res) =>{
     res.send('Server is started.')
 })
 
-app.get('/about', (req, res) =>{
-    res.send('About Page.')
+app.get('/about', (req, res, next) =>{
+    return next(new Error('Something went wrong.'))
+})
+
+app.use((err, req, res, next) =>{
+    res.status(500).send('Something went wrong.')
 })
 
 app.listen(3000)
