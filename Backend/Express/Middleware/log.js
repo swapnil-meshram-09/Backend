4c0f5fe55b803a8bbf37e5b03a292ec530047f59
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-10 16:29:27 +0530

    new commit

--- Backend/Express/Middleware/log.js
+++ Backend/Express/Middleware/log.js
@@ -2,11 +2,17 @@ import express from 'express'
 
 const app = express()
 
+app.use(()=>{
+    console.log('Middle ware');
+    
+})
+
+
 app.get('/', (req, res)=>{
     res.send('Server started.')
 })
 
-app.get('/about', ()=>{
+app.get('/about', (req, res)=>{
     res.send('About Page')
 })
 
