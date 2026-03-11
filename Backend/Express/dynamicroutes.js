93bb0fce6ebd4670c40f80dc3387b4e79d2e99d7
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-11 14:42:13 +0530

    new commit

--- Backend/Express/dynamicroutes.js
+++ Backend/Express/dynamicroutes.js
@@ -5,3 +5,13 @@ const app = express()
 app.get('/', (req, res) =>{
     console.log('Server is running.');
 })
+
+app.get('/user/:username', (req, res) =>{
+    console.log(req.params.username);
+})
+
+app.get('/user/:username/:id', (req, res) =>{
+    console.log(req.params.id);
+})
+
+app.listen(3000)
