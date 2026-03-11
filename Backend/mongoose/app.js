56161ccb195d44a2634f8431477e01141952c0fa
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-11 15:12:55 +0530

    new commit

--- Backend/mongoose/app.js
+++ Backend/mongoose/app.js
@@ -9,8 +9,27 @@ app.get('/', (req, res) =>{
     res.send('server is running')
 })
 
-app.get('/', () =>{
+app.get('/create', async (req, res) =>{
+    const createUser = await User.create({
+        name: 'name',
+        username: 'username',
+        email: 'test@test.com'
+    })
 
+    res.send(createUser)
+})
+
+app.get('/find', async (req, res) =>{
+    
+    res.send()
+})
+
+app.get('/update', async (req, res) =>{
+    res.send()
+})
+
+app.get('/delete', async (req, res) =>{
+    res.send()
 })
 
 app.listen(3000)
--- Backend/mongoose/db/db.js
+++ Backend/mongoose/db/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async() =>{
     try{
-        const connected = await mongoose.connect('')
+        const connected = await mongoose.connect(process.env.MONGODB_URI/user)
         console.log(connected);
         
     } catch(error){
