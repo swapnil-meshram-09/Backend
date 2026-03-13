b3e35bf0231fe7a926bae08b21c20a1a80b2709f
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-13 15:05:44 +0530

    new commit

--- Backend/Express/bcrypt/file.js
+++ Backend/Express/bcrypt/file.js
@@ -3,20 +3,23 @@ import bcrypt from 'bcrypt'
 
 const app = express()
 
-app.get('/', (req, res) =>{
-    bcrypt.genSalt(10, (err, salt) =>{
-        bcrypt.hash('password', salt, (err, hash) =>{
+const data = 'password'
+
+app.get('/', async (req, res) =>{
+    const salt = await bcrypt.genSalt(10, async (err, salt) =>{
+        const hashed = await bcrypt.hash(data, salt, (err, hash) =>{
+            // console.log(salt);
             console.log(hash);
         })
     })
-    req.send('Done')
+    res.send('Done')
 })
 
-app.get('/verify', (req, res) =>{
-    const result = bcrypt.compare('', 'password')
+app.get('/verify', async (req, res) =>{
+    const result = await bcrypt.compare('$2b$10$mYZCLWCMBBSDuz5WrIFOTOsQ8NvXYyT0NG.wynpyEilFucrNAzzYu', 'password')
     console.log(result);
     
-    res.send('Done')
+    res.send('Done 2')
 })
 
 app.listen(3000)
