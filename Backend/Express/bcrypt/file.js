b760c31904e5e422878b71f3492f543be7c54116
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-13 15:08:08 +0530

    new commit

--- Backend/Express/bcrypt/file.js
+++ Backend/Express/bcrypt/file.js
@@ -15,8 +15,11 @@ app.get('/', async (req, res) =>{
     res.send('Done')
 })
 
+
+const hashed = '$2b$10$n.dYV1OI9p/J7eX1XKM5mux5MkknM31kkVyKW76VIXl3C7wFSkkMm'
+
 app.get('/verify', async (req, res) =>{
-    const result = await bcrypt.compare('$2b$10$mYZCLWCMBBSDuz5WrIFOTOsQ8NvXYyT0NG.wynpyEilFucrNAzzYu', 'password')
+    const result = await bcrypt.compare(data, hashed)
     console.log(result);
     
     res.send('Done 2')
