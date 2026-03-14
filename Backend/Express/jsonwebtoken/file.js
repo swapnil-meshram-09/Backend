9ce9b1445a289dde0b9f3a4da46bbb379d6e6fca
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-14 13:17:15 +0530

    new commit

--- Backend/Express/jsonwebtoken/file.js
+++ Backend/Express/jsonwebtoken/file.js
@@ -1,13 +1,20 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 
 const app = express()
+app.use(cookieParser())
 
 app.get('/', (req, res) =>{
-    const token = jwt.sign({email: 'test@test.com'}, 'secret')
-    req.send('Done')
+    const token = jwt.sign({email2: 'test@test.com'}, 'ss')
+    res.cookie(token, 'email')
+    res.send('Done')
 })
 
+app.get('/verify', (req, res) =>{
+    console.log(req.cookies.token);
 
+    res.send('Done 2')
+})
 
 app.listen(3000)
