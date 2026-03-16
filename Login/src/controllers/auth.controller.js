f1cf4488772ea117d415b3c2c48fd1e138af7bd4
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-16 13:47:35 +0530

    new commit

--- Login/src/controllers/auth.controller.js
+++ Login/src/controllers/auth.controller.js
@@ -1,18 +1,20 @@
 import userModel from '../models/user.model.js'
 import crypto from 'crypto'
+import jwt from 'jsonwebtoken'
+import user from '../models/user.model.js'
 
 export async function register(req, res){
     const { username, email, password } = req.body
 
     const existingUser = await userModel.findOne({
-        or: [
+        $or: [
             { username },
             { email }
         ]
     })
 
     if(existingUser){
-        res.status(209).json({
+        res.status(409).json({
             message: 'User is already exists.'
         })
     }
@@ -24,6 +26,22 @@ export async function register(req, res){
         email,
         password: hashedPassword
     })
+
+    const token = jwt.sign({
+        id: userModel._id
+    }, 'secret', {
+        expiresIn: '1d'
+    })
+
+    res.status(201).json({
+        message: 'User register successfully.',
+        user: {
+            username,
+            email
+        },
+        token
+    })
+
 
 
 
