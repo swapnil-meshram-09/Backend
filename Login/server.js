39a2809262096961e75c10b1d22db1cde8b95246
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-16 13:03:01 +0530

    new commit

--- Login/server.js
+++ Login/server.js
@@ -1,58 +1,58 @@
 import app from './src/app.js'
 import connectDB from './src/config/db.js'
 import userModel from './src/models/user.model.js'
-import jwt from 'jsonwebtoken'
-import cookieParser from 'cookie-parser'
-import bcrypt from 'bcrypt'
-import express from 'express'
+// import jwt from 'jsonwebtoken'
+// import cookieParser from 'cookie-parser'
+// import bcrypt from 'bcrypt'
+
+connectDB()
 
 
 
-app.get('/', (req, res) =>{
-    res.render('index')
-})
+// app.get('/', (req, res) =>{
+//     res.render('index')
+// })
 
-app.post('/create', (req, res) =>{
-    const {username, email, password} = req.body
+// app.post('/create', (req, res) =>{
+//     const {username, email, password} = req.body
 
-    bcrypt.genSalt(10, (err, salt)=> {
-        bcrypt.hash(password, salt, async (err, hash) =>{
+//     bcrypt.genSalt(10, (err, salt)=> {
+//         bcrypt.hash(password, salt, async (err, hash) =>{
             
-           const createUser = await userModel.create({
-                username,
-                email, 
-                password: hash
-            })
-            res.send(createUser)
-        })
-    })
-})
+//            const createUser = await userModel.create({
+//                 username,
+//                 email, 
+//                 password: hash
+//             })
+//             res.send(createUser)
+//         })
+//     })
+// })
 
-app.get('/login', (req, res) =>{
-    res.render('login')
-})
+// app.get('/login', (req, res) =>{
+//     res.render('login')
+// })
 
-app.post('/login', async (req, res) =>{
-    const {email, password} = req.body
+// app.post('/login', async (req, res) =>{
+//     const {email, password} = req.body
 
-    const user = await userModel.findOne({email})
-    if(!user) return res.send('email or password is incorrect')
+//     const user = await userModel.findOne({email})
+//     if(!user) return res.send('email or password is incorrect')
 
-    // bcrypt.compare(password, user.password, (err, result) =>{
-    // if(err) return res.send('Incorrect password')
-    // if(! result) return res.send('Correct password')
+//     // bcrypt.compare(password, user.password, (err, result) =>{
+//     // if(err) return res.send('Incorrect password')
+//     // if(! result) return res.send('Correct password')
 
-     const result = await bcrypt.compare(password, user.password)
-     if(!result) return res.send('Incorrect Password')
+//      const result = await bcrypt.compare(password, user.password)
+//      if(!result) return res.send('Incorrect Password')
 
-     const token = jwt.sign({email: user.email}, 'secret')
-     res.cookie('token', token)
-     res.send('Login Successfully')
-})
+//      const token = jwt.sign({email: user.email}, 'secret')
+//      res.cookie('token', token)
+//      res.send('Login Successfully')
+// })
 
-
-app.get('/logout', (req, res) =>{
-    res.cookie('token', '')
-    res.redirect('/')
-})
+// app.get('/logout', (req, res) =>{
+//     res.cookie('token', '')
+//     res.redirect('/')
+// })
 
