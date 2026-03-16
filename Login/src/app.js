88ec559fddac6b9edc3c3ef0580ec037bfda3828
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-16 13:11:59 +0530

    new commit

--- Login/src/app.js
+++ Login/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(cookieParser())
 
