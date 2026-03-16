4cd2440ca5c76b88862ba386ba47dcf15e3e760e
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-16 13:09:19 +0530

    new commit

--- Login/src/app.js
+++ Login/src/app.js
@@ -1,13 +1,17 @@
 import express from 'express'
 import config from './config/config.js'
+import authRouter from './routes/auth.routes.js'
 
 const app = express()
 
 app.set('view engine', 'ejs')
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.use(cookieParser())
+
+app.use('/api/auth', authRouter)
 
 app.listen(config.PORT)
 
