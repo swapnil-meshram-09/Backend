50e0c9f6f77a36774818cf0edf9befc3bed49e8c
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-19 16:11:05 +0530

    new commit

--- Notes/server.js
+++ Notes/server.js
@@ -1,5 +1,5 @@
 import app from './src/app.js'
-import config from './src/config/config.js'
+import config from './src/configs/config.js'
 
 app.listen(config.PORT, () =>{
     console.log(`http://localhost:${config.PORT}`);
--- Notes/src/app.js
+++ Notes/src/app.js
@@ -1,5 +1,8 @@
 import express from 'express'
+import authRouter from './routes/auth.routes.js'
 
 const app = express()
+app.use(express.json())
+app.use('/api/auth', authRouter)
 
 export default app
