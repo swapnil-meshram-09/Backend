1b0fee605df413732b677d82c837025f5b6ee68b
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-20 14:24:15 +0530

    new commit

--- Notes/src/routes/notes.routes.js
+++ Notes/src/routes/notes.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
-import { createNote, getNotes } from '../controllers/notes.controllers.js'
+import { createNote, getNotes, deleteNote} from '../controllers/notes.controllers.js'
 
 const notesRouter = Router()
 
 notesRouter.post('/', createNote)
 
 notesRouter.get('/', getNotes)
+
+notesRouter.delete('/', deleteNote)
 
 export default notesRouter
