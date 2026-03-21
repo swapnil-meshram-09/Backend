af4e8f15757ad81fa35eb14d3309b4b4becc133e
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-21 15:48:42 +0530

    new commit

--- Spotify/server.js
+++ Spotify/server.js
@@ -1,5 +1,6 @@
 import app from './src/app.js'
+import config from './src/configs/config.js'
 
-app.listen(, () =>{
-    console.log(`http://localhost:${}`);
+app.listen(config.PORT, () =>{
+    console.log(`http://localhost:${config.PORT}`);
 })
