ed833812c826988ec4e14a15ba5707d15a6b6970
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-22 14:26:11 +0530

    new commit

--- Spotify/src/routes/auth.routes.js
+++ Spotify/src/routes/auth.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
-import { registerUser, getUsers } from '../controllers/auth.controllers.js'
+import { registerUser, getUsers, loginUser } from '../controllers/auth.controllers.js'
 
 const authRouter = Router()
 
 authRouter.post('/register', registerUser)
 
 authRouter.get('/get-users', getUsers)
+
+authRouter.post('/login-user', loginUser)
 
 export default authRouter
