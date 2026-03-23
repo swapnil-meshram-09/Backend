c4ee9241f2515a846beb3f3ef46c2861f64d12c1
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-23 13:55:43 +0530

    new commit

--- Spotify/src/routes/auth.routes.js
+++ Spotify/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { registerUser, getUsers, loginUser, deleteUser } from '../controllers/auth.controllers.js'
+import { registerUser, getUsers, loginUser, deleteUser, updateUser } from '../controllers/auth.controllers.js'
 
 const authRouter = Router()
 
@@ -10,5 +10,7 @@ authRouter.get('/get-users', getUsers)
 authRouter.post('/login-user', loginUser)
 
 authRouter.delete('/delete-user', deleteUser)
+
+authRouter.patch('/update-user', updateUser)
 
 export default authRouter
